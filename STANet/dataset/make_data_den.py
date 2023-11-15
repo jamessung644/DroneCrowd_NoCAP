f7b233682e40490fcbb6998672ab8e82f0d8c210
being0606 <onuhjin@gmail.com>
2023-11-16 00:20:42 +0900

    edit print()

--- STANet/dataset/make_data_den.py
+++ STANet/dataset/make_data_den.py
@@ -1,4 +1,5 @@
 import h5py
+from matplotlib import pyplot as plt
 import scipy
 import scipy.io as io
 import PIL.Image as Image
@@ -26,7 +27,7 @@ def gaussian_filter_density(gt):
     # query kdtree
     distances, locations = tree.query(pts, k=4)
 
-    print 'generate density...'
+    print('generate density...')
     for i, pt in enumerate(pts):
         pt2d = np.zeros(gt.shape, dtype=np.float32)
         pt2d[pt[1],pt[0]] = 1.
@@ -35,22 +36,22 @@ def gaussian_filter_density(gt):
         else:
             sigma = np.average(np.array(gt.shape))/2./2. #case: 1 point
         density += scipy.ndimage.filters.gaussian_filter(pt2d, sigma, mode='constant')
-    print 'done.'
+    print('done.')
     return density
 
 def process(idx, img_paths):
-	img_path = img_paths[idx]
-	mat = io.loadmat(img_path.replace('.jpg','.mat').replace('images','ground_truth').replace('img','GT_img'))
-	img = plt.imread(img_path)
-	k = np.zeros((img.shape[0],img.shape[1]))
-	gt = mat["image_info"][0,0][0,0][0]
-	for i in range(0,len(gt)):
-	    if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
-	        k[int(gt[i][1]),int(gt[i][0])]=1
-	k = gaussian_filter_density(k)
+    img_path = img_paths[idx]
+    mat = io.loadmat(img_path.replace('.jpg','.mat').replace('images','ground_truth').replace('img','GT_img'))
+    img = plt.imread(img_path)
+    k = np.zeros((img.shape[0],img.shape[1]))
+    gt = mat["image_info"][0,0][0,0][0]
+    for i in range(0,len(gt)):
+        if int(gt[i][1])<img.shape[0] and int(gt[i][0])<img.shape[1]:
+            k[int(gt[i][1]),int(gt[i][0])]=1
+    k = gaussian_filter_density(k)
 
-	with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground_truth'), 'w') as hf:
-		    hf['density'] = k
+    with h5py.File(img_path.replace('.jpg','.h5').replace('images','ground_truth'), 'w') as hf:
+        hf['density'] = k
 
 
 part_train = os.path.join('train_data','images')
